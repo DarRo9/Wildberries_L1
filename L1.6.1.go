5631feb8622bc77d4d98ce137a404091d5f3b5d8
DarRo9 <124833469+DarRo9@users.noreply.github.com>
2023-09-14 21:53:55 +0300

    Add files via upload

---
+++ L1.6.1.go
@@ -0,0 +1,33 @@
+//Реализовать все возможные способы остановки выполнения горутины. 
+
+package main
+
+import (
+	"sync"
+	"fmt"
+	"time"
+)
+
+func main(){
+	var wg sync.WaitGroup
+	stop := make(chan string)
+	//wg чтобы главная горутина не завершилась раньше остальных
+	wg.Add(1)
+	//Горутина
+	go func(){
+		defer wg.Done()
+		for{
+		select{
+			case <-stop:
+				fmt.Println("Завершение горутины")
+				return
+			default:
+				fmt.Println("Горутина работает")
+			}
+		}
+	}()
+	time.Sleep(1 * time.Second)
+	//Канал для остановки горутины
+	stop <- "stop"
+	wg.Wait()
+}
